7e8ab8114205237469af73f7c3931095ca28093b
Daniel McNelis <dwm253@gmail.com>
2021-10-13 18:38:32 -0400

    migrations

---
+++ apps/api/knexfile.ts
@@ -0,0 +1,31 @@
+// tslint:disable-next-line
+require('dotenv').config({ path: '../../.env' })
+import { knex as connect } from 'knex'
+
+module.exports = async () => {
+    const config = {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: './pg/migrations',
+            tableName: 'migrations'
+        },
+        seeds: {
+            directory: './pg/seeds'
+        }
+    }
+
+    // const knex = connect(config)
+    // await knex.raw('CREATE DATABASE IF NOT EXISTS ??', process.env.DATABASE_URL)
+
+    return {
+        default: {
+            ...config,
+            connection: process.env.DATABASE_URL
+        }
+    }
+}
---
+++ apps/api/pg/migrations/20211013220834_add-users-table.ts
@@ -0,0 +1,31 @@
+
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.hasTable('users').then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable('users', (table) => {
+                table.string('id', 22).notNullable().primary()
+                table.string('email', 128).notNullable()
+                table.string('displayName', 128).nullable().defaultTo(null)
+                table.string('username', 128).notNullable()
+                table.string('passwordHash', 72).notNullable()
+                table.timestamp('createdAt').defaultTo(knex.fn.now())
+                table.timestamp('updatedAt').defaultTo(knex.fn.now())
+                table.unique(['email'], 'email')
+                table.unique(['username'], 'username')
+                return table
+            })
+        }
+        return
+    })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.hasTable('users').then((exists) => {
+        if (exists) {
+            return knex.schema.dropTable('users')
+        }
+        return
+    })
+}
